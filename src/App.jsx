3a34395527341f12648f7cde2751b7fbd3736259
mizanm4yourslab <mizan@m4yours.com>
2023-10-15 04:58:01 +0600

    tiny change

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Home from "./pages/Home";
-
 const App = () => {
   return (
     <Router>
